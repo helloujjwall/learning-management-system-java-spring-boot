3e2f17b7b316d6bba30701c1ccf92ff51c0cbda4
Ujjwal Singh <mrujjwal03@gmail.com>
2024-10-12 21:51:20 +0530

    Upload nouapp folder

---
+++ nouapp/nouapp/src/main/java/com/app/nouapp/service/AdminLoginRepository.java
@@ -0,0 +1,9 @@
+package com.app.nouapp.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.nouapp.model.AdminLogin;
+
+public interface AdminLoginRepository extends JpaRepository< AdminLogin, String>{
+
+}
