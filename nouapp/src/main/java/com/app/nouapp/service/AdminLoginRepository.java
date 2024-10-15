b4480a96af5030df77b797776981a1f02a86f60a
Ujjwal Singh <mrujjwal03@gmail.com>
2024-10-15 23:11:50 +0530

    Remove all

--- nouapp/src/main/java/com/app/nouapp/service/AdminLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.nouapp.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.nouapp.model.AdminLogin;
-
-public interface AdminLoginRepository extends JpaRepository< AdminLogin, String>{
-
-}
--- nouapp/src/main/java/com/app/nouapp/service/EnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.nouapp.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.nouapp.model.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry, Integer>{
-
-}
